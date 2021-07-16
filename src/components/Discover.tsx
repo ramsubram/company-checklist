2856f7ce485abf6aed49232d8bb43181de81251e
ramsubram <ramanans@protonmail.com>
2021-07-16 12:52:24 +0200

    Layout overlapping fixed

--- src/components/Discover.tsx
+++ src/components/Discover.tsx
@@ -9,48 +9,44 @@ const h1 = css`
   font-family: 'Lobster Two', cursive;
 `;
 
-const ul = css`
-  margin-left: 30px;
+const container = css`
+  display: flex;
+  flex-direction: column;
+  position: relative;
 `;
 
-const container = css``;
-
 const checkmark = css`
-  position: absolute;
-  top: 0;
-  left: 0;
+  display: flex;
+  flex-direction: column;
   height: 25px;
   width: 25px;
-  background-color: green;
+  top: 1;
+  left: 1;
+  height: 25px;
+  width: 25px;
+  background-color: #26a859;
 `;
 
 const Discover = () => {
   return (
     <div>
       <h1 css={h1}>Discover your next coworker</h1>
-
-      <div css={ul}>
-        <ul>
-          <label className="container" css={container}>
-            <input type="checkbox" />
-            <span css={checkmark}></span>
-            Upload logo
-          </label>
-        </ul>
-        <ul>
-          <label className="container">
-            <input type="checkbox" />
-            <span css={checkmark}></span>
-            Add description
-          </label>
-        </ul>
-        <ul>
-          <label className="container">
-            <input type="checkbox" />
-            <span css={checkmark}></span>
-            Upgrade
-          </label>
-        </ul>
+      <div>
+        <label className="container" css={container}>
+          <input type="checkbox" />
+          <span css={checkmark}></span>
+          Upload logo
+        </label>
+        <label className="container">
+          <input type="checkbox" />
+          <span css={checkmark}></span>
+          Add description
+        </label>
+        <label className="container">
+          <input type="checkbox" />
+          <span css={checkmark}></span>
+          Upgrade
+        </label>
       </div>
     </div>
   );
